58ef18b897c61f5e4ec8994bf24157448e4046d3
Matthias Buchholz <matthias@afkm.io>
2018-02-09 19:21:15 +0100

    data items

--- src/app/meta-cloud/components/meta-cloud/meta-cloud.component.spec.ts
+++ src/app/meta-cloud/components/meta-cloud/meta-cloud.component.spec.ts
@@ -1,10 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MetaCloudComponent } from './meta-cloud.component';
 
+const TEST_DATA:any[] = [
+  {
+    index: 0,
+    label: "Hello",
+    data: "Hello"
+  },
+  {
+    index: 1,
+    label: "World",
+    data: "World"
+  },
+  {
+    index: 2,
+    label: "Foobar",
+    data: "Foobar"
+  }
+]
+
 describe('MetaCloudComponent', () => {
-  let component: MetaCloudComponent;
-  let fixture: ComponentFixture<MetaCloudComponent>;
+  let component: MetaCloudComponent<any>;
+  let fixture: ComponentFixture<MetaCloudComponent<any>>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +34,32 @@ describe('MetaCloudComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MetaCloudComponent);
     component = fixture.componentInstance;
+    component.dataItems = TEST_DATA
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should call "onButtonClick"', fakeAsync(() => {
+
+    spyOn(component,'onButtonClick')
+
+    let button:HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('button')
+    button.click()    
+
+
+    expect(component.onButtonClick).toHaveBeenCalled()
+
+    tick(300)
+    
+    expect(component.selectedItems).toBe(jasmine.any(Array))
+    expect(component.selectedItems.length).toEqual(1)
+
+  }))
+
+
+
 });
--- src/app/meta-cloud/components/meta-cloud/meta-cloud.component.ts
+++ src/app/meta-cloud/components/meta-cloud/meta-cloud.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { IDataItem } from '../../interfaces/data-item'
+import { IComponentOptions } from '../../interfaces/options'
+
+
 
 @Component({
   selector: 'kio-meta-cloud',
   templateUrl: './meta-cloud.component.html',
   styleUrls: ['./meta-cloud.component.css']
 })
-export class MetaCloudComponent implements OnInit {
+export class MetaCloudComponent <T> implements OnInit, IComponentOptions {
 
   constructor() { }
 
+  @Input() selectedItems:IDataItem<T>[]=[]
+
+  @Input('items') dataItems:IDataItem<T>[]=[]
+
+  @Input() multiple:boolean=true
+
+  @Output() select:EventEmitter<IDataItem<T>[]>=new EventEmitter()
+
   ngOnInit() {
+
+  }
+
+  public isItemSelected ( item:IDataItem<T> ):boolean {
+
+    return this.selectedItems.findIndex ( (selItem:IDataItem<T>) => selItem.index === item.index ) > -1
+
+  }
+
+  public onButtonClick(event:Event,item:IDataItem<T>){
+
+    if ( this.isItemSelected(item) ) {
+
+      this._updateSelection(this.selectedItems.filter ( selItem => selItem.index !== item.index ))
+
+    } else {
+
+      if ( this.multiple ) {
+
+        this._updateSelection(this.selectedItems.concat([item]))
+
+      } else {
+
+        this._updateSelection([item])
+
+      }
+
+    }
+
+  }
+
+  private _updateSelection ( items:IDataItem<T>[] ) {
+
+    this.selectedItems = items.slice()
+    this.select.emit(items)
+
   }
 
 }
