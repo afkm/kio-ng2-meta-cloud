ef41b47919f1acf078148e91da53c4bd17ca78e0
Matthias Buchholz <matthias@afkm.io>
2018-02-09 19:21:27 +0100

    test app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IDataItem, IComponentOptions } from './meta-cloud/meta-cloud.module'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  testData:IDataItem<string>[]=[
+    {
+      index: 0,
+      label: "Hello",
+      data: "Hello"
+    },
+    {
+      index: 1,
+      label: "World",
+      data: "World"
+    },
+    {
+      index: 2,
+      label: "Foobar",
+      data: "Foobar"
+    }
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MetaCloudModule } from './meta-cloud/meta-cloud.module'
+
 
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MetaCloudModule
   ],
   providers: [],
   bootstrap: [AppComponent]
